154f41f13ae103e9e46860968eec920e17f402de
yyh12366 <2359070149@qq.com>
2026-01-17 01:50:43 +0800

    Allow all hosts in Vite server

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
+    allowedHosts: 'all',
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:3000',
